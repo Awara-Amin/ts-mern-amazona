6564c2b22a18961dac7fd2504fc0cc3f92a2a07f
AaA <awarageochemistry@gmail.com>
2024-09-11 19:06:55 +0100

    Video-17-Add-To-Cart-In-Product-Page

--- frontend/src/pages/ProductPage.tsx
+++ frontend/src/pages/ProductPage.tsx
@@ -1,14 +1,16 @@
 // import React from "react"
+import { useContext } from "react"
 import { Badge, Button, Card, Col, ListGroup, Row } from "react-bootstrap"
 import { Helmet } from "react-helmet-async"
-
-import { useParams } from "react-router-dom"
 import LoadingBox from "../components/LoadingBox"
 import MessageBox from "../components/MessageBox"
 import Rating from "../components/Rating"
 import { useGetProductDetailsBySlugQuery } from "../hooks/productHooks"
 import { ApiError } from "../types/ApiError"
-import { getError } from "../utils"
+import { Store } from "../Store"
+import { convertProductToCartItem, getError } from "../utils"
+import { useNavigate, useParams } from "react-router-dom"
+import { toast } from "react-toastify"
 
 export default function ProductPage() {
   const params = useParams()
@@ -18,6 +20,26 @@ export default function ProductPage() {
     isLoading,
     error,
   } = useGetProductDetailsBySlugQuery(slug!)
+
+  const { state, dispatch } = useContext(Store)
+  const { cart } = state
+
+  const navigate = useNavigate()
+
+  const addToCartHandler = () => {
+    const existItem = cart.cartItems.find((x) => x._id === product!._id)
+    const quantity = existItem ? existItem.quantity + 1 : 1
+    if (product!.countInStock < quantity) {
+      toast.warn("Sorry. Product is out of stock")
+      return
+    }
+    dispatch({
+      type: "CART_ADD_ITEM",
+      payload: { ...convertProductToCartItem(product!), quantity },
+    })
+    toast.success("Product added to the cart")
+    navigate("/cart")
+  }
 
   return isLoading ? (
     <LoadingBox />
