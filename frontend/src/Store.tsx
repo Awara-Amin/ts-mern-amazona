efe3335ca690ce978c80a3d862f2e7eb5d9b3bf6
AaA <awarageochemistry@gmail.com>
2024-09-12 10:51:23 +0100

    Video-19-Implement-Remove-From-Cart

--- frontend/src/Store.tsx
+++ frontend/src/Store.tsx
@@ -1,12 +1,9 @@
 import React from "react"
 import { Cart, CartItem } from "./types/Cart"
-
-//typeScript part
 type AppState = {
   mode: string
   cart: Cart
 }
-
 const initialState: AppState = {
   mode: localStorage.getItem("mode")
     ? localStorage.getItem("mode")!
@@ -14,7 +11,6 @@ const initialState: AppState = {
       window.matchMedia("(prefers-color-scheme: dark)").matches
     ? "dark"
     : "light",
-  ////
   cart: {
     cartItems: localStorage.getItem("cartItems")
       ? JSON.parse(localStorage.getItem("cartItems")!)
@@ -31,19 +27,15 @@ const initialState: AppState = {
     totalPrice: 0,
   },
 }
-
-// type Action = { type: "SWITCH_MODE" }
 type Action =
   | { type: "SWITCH_MODE" }
-  //
   | { type: "CART_ADD_ITEM"; payload: CartItem }
+  | { type: "CART_REMOVE_ITEM"; payload: CartItem }
 
 function reducer(state: AppState, action: Action): AppState {
   switch (action.type) {
     case "SWITCH_MODE":
-      // return { mode: state.mode === "dark" ? "light" : "dark" }
       return { ...state, mode: state.mode === "dark" ? "light" : "dark" }
-    //
     case "CART_ADD_ITEM":
       const newItem = action.payload
       const existItem = state.cart.cartItems.find(
@@ -54,29 +46,32 @@ function reducer(state: AppState, action: Action): AppState {
             item._id === existItem._id ? newItem : item
           )
         : [...state.cart.cartItems, newItem]
-
       localStorage.setItem("cartItems", JSON.stringify(cartItems))
 
       return { ...state, cart: { ...state.cart, cartItems } }
+
+    case "CART_REMOVE_ITEM": {
+      const cartItems = state.cart.cartItems.filter(
+        (item: CartItem) => item._id !== action.payload._id
+      )
+      localStorage.setItem("cartItems", JSON.stringify(cartItems))
+      return { ...state, cart: { ...state.cart, cartItems } }
+    }
+
     default:
       return state
   }
 }
-
 const defaultDispatch: React.Dispatch<Action> = () => initialState
-
 const Store = React.createContext({
   state: initialState,
   dispatch: defaultDispatch,
 })
-
 function StoreProvider(props: React.PropsWithChildren<{}>) {
   const [state, dispatch] = React.useReducer<React.Reducer<AppState, Action>>(
     reducer,
     initialState
   )
-
   return <Store.Provider value={{ state, dispatch }} {...props} />
 }
-
 export { Store, StoreProvider }
